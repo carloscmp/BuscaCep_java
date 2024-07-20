1a642a9920c11e25dcc99bcc06d316890e10f011
carloscmp <99369400+carloscmp@users.noreply.github.com>
2024-07-20 18:36:53 -0300

    Finalização do projeto

--- BuscaCep/src/cep/Cep.java
+++ BuscaCep/src/cep/Cep.java
@@ -1,30 +1,40 @@
 package cep;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.Toolkit;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
-import java.awt.Cursor;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.Iterator;
+
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import Atxy2k.CustomTextField.RestrictedTextField;
 
 public class Cep extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtCep;
+	private JTextField txtEndereco;
+	private JTextField txtBairro;
+	private JTextField txtCidade;
+	private JComboBox cboUf;
+	private JLabel lblStatus;
 
 	/**
 	 * Launch the application.
@@ -56,70 +66,153 @@ public class Cep extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("CEP");
 		lblNewLabel.setBounds(10, 26, 46, 14);
 		contentPane.add(lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setBounds(73, 23, 119, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
+
+		txtCep = new JTextField();
+		txtCep.setBounds(73, 23, 119, 20);
+		contentPane.add(txtCep);
+		txtCep.setColumns(10);
+
 		JLabel lblNewLabel_1 = new JLabel("Endereço");
-		lblNewLabel_1.setBounds(10, 65, 46, 14);
+		lblNewLabel_1.setBounds(10, 65, 63, 14);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Bairro");
 		lblNewLabel_2.setBounds(10, 103, 46, 14);
 		contentPane.add(lblNewLabel_2);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("Cidade");
 		lblNewLabel_3.setBounds(10, 143, 46, 14);
 		contentPane.add(lblNewLabel_3);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(73, 62, 245, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(73, 100, 119, 20);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(73, 140, 96, 20);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
-		
+
+		txtEndereco = new JTextField();
+		txtEndereco.setBounds(73, 62, 245, 20);
+		contentPane.add(txtEndereco);
+		txtEndereco.setColumns(10);
+
+		txtBairro = new JTextField();
+		txtBairro.setBounds(73, 100, 245, 20);
+		contentPane.add(txtBairro);
+		txtBairro.setColumns(10);
+
+		txtCidade = new JTextField();
+		txtCidade.setBounds(73, 140, 168, 20);
+		contentPane.add(txtCidade);
+		txtCidade.setColumns(10);
+
 		JLabel lblNewLabel_4 = new JLabel("UF");
 		lblNewLabel_4.setBounds(251, 143, 46, 14);
 		contentPane.add(lblNewLabel_4);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(307, 139, 57, 22);
-		contentPane.add(comboBox);
-		
-		JButton btnNewButton = new JButton("Limpar");
-		btnNewButton.addActionListener(new ActionListener() {
+
+		cboUf = new JComboBox();
+		cboUf.setModel(new DefaultComboBoxModel(
+				new String[] { "", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA",
+						"PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO" }));
+		cboUf.setBounds(281, 139, 83, 22);
+		contentPane.add(cboUf);
+
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				limpar();
 			}
 		});
-		btnNewButton.setBounds(10, 227, 89, 23);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Buscar");
-		btnNewButton_1.setBounds(251, 22, 89, 23);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("");
-		btnNewButton_2.setToolTipText("Sobre");
-		btnNewButton_2.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
-		btnNewButton_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_2.setBorder(null);
-		btnNewButton_2.setBackground(SystemColor.control);
-		btnNewButton_2.setBounds(376, 11, 48, 48);
-		contentPane.add(btnNewButton_2);
+		btnLimpar.setBounds(10, 227, 89, 23);
+		contentPane.add(btnLimpar);
+
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (txtCep.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "Informe o Cep");
+					txtCep.requestFocus();
+				} else {
+					buscarCep();
+				}
+			}
+		});
+		btnBuscar.setBounds(251, 22, 89, 23);
+		contentPane.add(btnBuscar);
+
+		/* uso da biblioteca atx2k para validação do campo cep */
+		RestrictedTextField validar = new RestrictedTextField(txtCep);
+
+		JButton btnSobre = new JButton("");
+		btnSobre.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Sobre sobre = new Sobre();
+				sobre.setVisible(true);
+			}
+		});
+		btnSobre.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
+		btnSobre.setToolTipText("Projeto");
+		btnSobre.setBorder(null);
+		btnSobre.setBackground(SystemColor.control);
+		btnSobre.setBounds(376, 11, 48, 48);
+		contentPane.add(btnSobre);
+
+		lblStatus = new JLabel("");
+		lblStatus.setBounds(204, 26, 20, 20);
+		contentPane.add(lblStatus);
+		validar.setOnlyNums(true);
+		validar.setLimit(8);
 	}
+
+	private void buscarCep() {
+		String logradouro = "";
+		String tipoLogradouro = "";
+		String resultado = null;
+		String cep = txtCep.getText();
+
+		try {
+			URL url = new URL("http://cep.republicavirtual.com.br/web_cep.php?cep=" + cep + "&formato=xml");
+			SAXReader xml = new SAXReader();
+			Document documento = xml.read(url);
+			Element root = documento.getRootElement();
+			for (Iterator<Element> it = root.elementIterator(); it.hasNext();) {
+				Element element = it.next();
+				if (element.getQualifiedName().equals("cidade")) {
+					txtCidade.setText(element.getText());
+				}
+				if (element.getQualifiedName().equals("bairro")) {
+					txtBairro.setText(element.getText());
+				}
+				if (element.getQualifiedName().equals("uf")) {
+					cboUf.setSelectedItem(element.getText());
+				}
+				if (element.getQualifiedName().equals("logradouro")) {
+					logradouro = element.getText();
+				}
+				if (element.getQualifiedName().equals("tipo_logradouro")) {
+					tipoLogradouro = element.getText();
+				}
+				if (element.getQualifiedName().equals("resultado")) {
+					resultado = element.getText();
+					if (resultado.equals("1")) {
+						lblStatus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/check.png")));
+					} else {
+						JOptionPane.showMessageDialog(null, "CEP não encontrado");
+					}
+				}
+				// Setar campo endereço
+				txtEndereco.setText(tipoLogradouro + " " + logradouro);
+
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	private void limpar() {
+		txtBairro.setText("");
+		txtCep.setText("");
+		txtCidade.setText(null);
+		txtEndereco.setText(null);
+		cboUf.setSelectedItem(null);
+		txtCep.requestFocus();
+		lblStatus.setIcon(null);
+	}
+
 }
--- BuscaCep/src/cep/Sobre.java
+++ BuscaCep/src/cep/Sobre.java
@@ -1,16 +1,20 @@
 package cep;
 
-import java.awt.EventQueue;
-
-import javax.swing.JDialog;
-import java.awt.Toolkit;
-import javax.swing.JLabel;
-import java.awt.SystemColor;
-import javax.swing.JButton;
-import javax.swing.ImageIcon;
 import java.awt.Cursor;
-import java.awt.event.ActionListener;
+import java.awt.Desktop;
+import java.awt.EventQueue;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Sobre extends JDialog {
 
@@ -37,51 +41,75 @@ public class Sobre extends JDialog {
 	 * Create the dialog.
 	 */
 	public Sobre() {
+		setModal(true);
 		setResizable(false);
 		setTitle("Sobre");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Sobre.class.getResource("/img/home.png")));
-		setBounds(100, 100, 450, 300);
+		setBounds(150, 150, 450, 300);
 		getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Buscar CEP - Versão 1.0");
 		lblNewLabel.setBounds(10, 11, 228, 14);
 		getContentPane().add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("@Author Carlos Marques");
 		lblNewLabel_1.setBounds(10, 36, 228, 14);
 		getContentPane().add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Web Service:");
 		lblNewLabel_2.setBounds(10, 61, 128, 14);
 		getContentPane().add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("markdev.com.br");
-		lblNewLabel_3.setForeground(SystemColor.textHighlight);
-		lblNewLabel_3.setBounds(106, 61, 160, 14);
-		getContentPane().add(lblNewLabel_3);
-		
-		JButton btnNewButton = new JButton("");
-		btnNewButton.setToolTipText("Projeto");
-		btnNewButton.setIcon(new ImageIcon(Sobre.class.getResource("/img/github.png")));
-		btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton.setBorder(null);
-		btnNewButton.setBackground(SystemColor.control);
-		btnNewButton.setBounds(90, 202, 48, 48);
-		getContentPane().add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+
+		JLabel lblWebService = new JLabel("markdev.com.br");
+		lblWebService.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				link("https://republicavirtual.com.br/");
 			}
 		});
-		btnNewButton_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_1.setIcon(new ImageIcon(Sobre.class.getResource("/img/youtube.png")));
-		btnNewButton_1.setToolTipText("Playlist");
-		btnNewButton_1.setBorder(null);
-		btnNewButton_1.setBackground(SystemColor.control);
-		btnNewButton_1.setBounds(22, 202, 48, 48);
-		getContentPane().add(btnNewButton_1);
+		lblWebService.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblWebService.setForeground(SystemColor.textHighlight);
+		lblWebService.setBounds(132, 61, 160, 14);
+		getContentPane().add(lblWebService);
+
+		JButton btnGithub = new JButton("");
+		btnGithub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				link("https://github.com/carloscmp");
+			}
+		});
+		btnGithub.setToolTipText("Projeto");
+		btnGithub.setIcon(new ImageIcon(Sobre.class.getResource("/img/github.png")));
+		btnGithub.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnGithub.setBorder(null);
+		btnGithub.setBackground(SystemColor.control);
+		btnGithub.setBounds(90, 202, 48, 48);
+		getContentPane().add(btnGithub);
+
+		JButton btnYoutube = new JButton("");
+		btnYoutube.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				link("https://www.youtube.com/@carlos_marques");
+			}
+		});
+		btnYoutube.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnYoutube.setIcon(new ImageIcon(Sobre.class.getResource("/img/youtube.png")));
+		btnYoutube.setToolTipText("Playlist");
+		btnYoutube.setBorder(null);
+		btnYoutube.setBackground(SystemColor.control);
+		btnYoutube.setBounds(22, 202, 48, 48);
+		getContentPane().add(btnYoutube);
 
 	}
 
+	// método responsavel por abrir links externos
+	private void link(String site) {
+		Desktop desktop = Desktop.getDesktop();
+		try {
+			URI uri = new URI(site);
+			desktop.browse(uri);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
